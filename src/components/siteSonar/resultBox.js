aee9aa5f6eebaac6439bc3c26e19c186741b4272
KalanaDananjaya <kalana.16@cse.mrt.ac.lk>
2020-07-23 13:33:59 +0530

    Create result box

--- src/components/siteSonar/resultBox.js
+++ src/components/siteSonar/resultBox.js
@@ -15,42 +15,129 @@ import {
 
 import { dashboardEmailStatisticsChart } from "../Charts/PieChart.js";
 
-class ResultBox extends React.Component {
-    constructor(props){
-        super(props);
-        this.test = ['a',2,'5']
-    }
+const ResultBox = (props) => {
+  
+  const test = () => {
 
-    render() {
-        return (
-            <Col md="4">
-              <Card>
-                <CardHeader>
-                  <CardTitle tag="h5">Grid Config</CardTitle>
-                  <p className="card-category">Sitename</p>
-                </CardHeader>
+  }
+  
+  if (props.showResult){
+    return (
+      <div>
+        <Row>
+            <Col lg="3" md="6" sm="6">
+              <Card className="card-stats">
                 <CardBody>
-                  <Pie
-                    data={dashboardEmailStatisticsChart.data}
-                    options={dashboardEmailStatisticsChart.options}
-                  />
+                  <Row>
+                    <Col md="4" xs="5">
+                      <div className="icon-big text-center icon-warning">
+                        <i className="nc-icon nc-globe text-warning" />
+                      </div>
+                    </Col>
+                    <Col md="8" xs="7">
+                      <div className="numbers">
+                        <p className="card-category">Submitted Jobs</p>
+                        <CardTitle tag="p">{props.searchData.submitted_jobs}</CardTitle>
+                        <p />
+                      </div>
+                    </Col>
+                  </Row>
                 </CardBody>
                 <CardFooter>
-                  <div className="legend">
-                    <i className="fa fa-circle text-primary" /> Opened{" "}
-                    <i className="fa fa-circle text-warning" /> Read{" "}
-                    <i className="fa fa-circle text-danger" /> Deleted{" "}
-                    <i className="fa fa-circle text-gray" /> Unopened
-                  </div>
                   <hr />
                   <div className="stats">
-                    <i className="fa fa-calendar" /> Number of emails sent
+                    <i className="fas fa-sync-alt" /> Update Now
                   </div>
                 </CardFooter>
               </Card>
             </Col>
-        );
-    }
+            <Col lg="3" md="6" sm="6">
+              <Card className="card-stats">
+                <CardBody>
+                  <Row>
+                    <Col md="4" xs="5">
+                      <div className="icon-big text-center icon-warning">
+                        <i className="nc-icon nc-money-coins text-success" />
+                      </div>
+                    </Col>
+                    <Col md="8" xs="7">
+                      <div className="numbers">
+                        <p className="card-category">Nodes Covered</p>
+                        <CardTitle tag="p">{props.searchData.completed_jobs}</CardTitle>
+                        <p />
+                      </div>
+                    </Col>
+                  </Row>
+                </CardBody>
+                <CardFooter>
+                  <hr />
+                  <div className="stats">
+                    <i className="far fa-calendar" /> Last day
+                  </div>
+                </CardFooter>
+              </Card>
+            </Col>
+            <Col lg="3" md="6" sm="6">
+              <Card className="card-stats">
+                <CardBody>
+                  <Row>
+                    <Col md="4" xs="5">
+                      <div className="icon-big text-center icon-warning">
+                        <i className="nc-icon nc-vector text-danger" />
+                      </div>
+                    </Col>
+                    <Col md="8" xs="7">
+                      <div className="numbers">
+                        <p className="card-category">Matching Nodes</p>
+                        <CardTitle tag="p">{props.searchData.matching_jobs}</CardTitle>
+                        <p />
+                      </div>
+                    </Col>
+                  </Row>
+                </CardBody>
+                <CardFooter>
+                  <hr />
+                  <div className="stats">
+                    <i className="far fa-clock" /> In the last hour
+                  </div>
+                </CardFooter>
+              </Card>
+            </Col>
+            <Col lg="3" md="6" sm="6">
+              <Card className="card-stats">
+                <CardBody>
+                  <Row>
+                    <Col md="4" xs="5">
+                      <div className="icon-big text-center icon-warning">
+                        <i className="nc-icon nc-favourite-28 text-primary" />
+                      </div>
+                    </Col>
+                    <Col md="8" xs="7">
+                      <div className="numbers">
+                        <p className="card-category">Matching Percentage</p>
+                        <CardTitle tag="p">{Math.round((props.searchData.matching_jobs/props.searchData.completed_jobs)*100)}%</CardTitle>
+                        <p />
+                      </div>
+                    </Col>
+                  </Row>
+                </CardBody>
+                <CardFooter>
+                  <hr />
+                  <div className="stats">
+                    <i className="fas fa-sync-alt" /> Update now
+                  </div>
+                </CardFooter>
+              </Card>
+            </Col>
+          </Row>
+      </div>
+    );
+  }
+  else{
+    return (<a></a>)
+  }
+  
 }
+
 
 export default ResultBox;
